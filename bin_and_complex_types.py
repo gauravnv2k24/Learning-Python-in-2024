bb36770523a709655addcd9dfd01b84dcf531e78
Gaurav N V <gauravvenkat2k22@icloud.com>
2024-06-16 11:05:49 +0530

    feat: binary and complex types
    Added a code file to represent how to represent a number as complex or binary is added.
    binary to decimal is also added in this code.

---
+++ bin_and_complex_types.py
@@ -0,0 +1,16 @@
+# complex number
+# numbers in python can be real or complex
+a = 1 + 2j
+b = 3 + 4j
+print(a + b) # (4+6j)
+
+
+# bin() function
+# bin() function returns the binary representation of a number
+# bin(x) returns the binary representation of x
+# x must be an integer
+
+print(bin(10)) # 0b1010 => 1010 is the binary representation of 10
+
+# convert binary to decimal
+print(int('0b1010', 2)) # 10
