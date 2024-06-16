39988f8bebd80e2e3aab54a76526e7e898e4b311
Gaurav N V <gauravvenkat2k22@icloud.com>
2024-06-16 11:23:02 +0530

    feat: augmented assignment in python
    Add a code file that show how augmented assignment done in python

---
+++ augmented_assignment.py
@@ -0,0 +1,30 @@
+# augmented assignment
+
+# Augmented assignment is a shorthand syntax for updating the value of a variable.
+# It is also known as in-place assignment.
+
+# The following table shows the augmented assignment operators in Python:
+
+# Operator	Example	Equivalent
+# +=	x += 5	x = x + 5
+# -=	x -= 5	x = x - 5
+# *=	x *= 5	x = x * 5
+# /=	x /= 5	x = x / 5
+
+
+# Example: Augmented Assignment
+x = 5
+x += 3
+
+print(x) # 8
+
+x -= 3
+print(x) # 5
+
+x *= 3
+print(x) # 15
+
+x /= 3
+print(x) # 5.0
+
+# The augmented assignment operators are faster than the normal arithmetic operators.
