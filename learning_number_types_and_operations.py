0b5d1e900e61c36059324fa6022b5d56dafc0d88
Gaurav N V <gauravvenkat2k22@icloud.com>
2024-06-16 10:41:26 +0530

    feat: learning number types and their operations
    This files shows the number types like int and float learnt. Additionally, Arithmetic operations which can be performed on them are also included

---
+++ learning_number_types_and_operations.py
@@ -0,0 +1,35 @@
+# int: numbers without decimal points (integers) 2^64 - 1 limit
+
+#this is an integer
+int_num = 5
+
+#this prints <class 'int'>
+print(type(int_num))
+
+#float: numbers with decimal points
+#more memory is required to store floats than integers
+
+#this is a float
+float_num = 0.5820
+
+#this prints <class 'float'>
+print(type(float_num))
+
+# operators
+'''
++: addition
+-: subtraction
+*: multiplication
+/: division
+//: floor division
+%: modulo
+**: exponentiation
+'''
+print(5 + 2) #7
+print(5 - 2) #3
+print(5 * 2) #10
+print(5 / 2) #2.5
+print(5 // 2) #2
+print(5 % 2) #1
+print(5 ** 2) #25
+
