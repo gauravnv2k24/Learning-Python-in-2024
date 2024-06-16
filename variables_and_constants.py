0cdda782d16d74d62a377eedf5036a7f6ec39a45
Gaurav N V <gauravvenkat2k22@icloud.com>
2024-06-16 11:18:20 +0530

    feat: variables and constants
    This added file contain how variable and constants are declared and rules for naming them and all possible assignments of them.

---
+++ variables_and_constants.py
@@ -0,0 +1,25 @@
+# variable
+'''
+rules for naming variables
+1. A variable name must start with a letter or the underscore character
+2. A variable name cannot start with a number
+3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+4. Variable names are case-sensitive (name, Name, and NAME are three different variables)
+'''
+name = "John"
+print(name) # John
+
+# multiple assignment
+x, y, z = 1, 2, 3
+print(x) # 1
+print(y) # 2
+print(z) # 3
+
+
+# constant
+'''
+constants are written in uppercase letters
+(However, this is not mandatory, but it is a common practice to differentiate constants from variables)
+'''
+PI = 3.14
+print(PI) # 3.14
