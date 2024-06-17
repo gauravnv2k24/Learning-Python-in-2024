213c0784719b95f9d186dc5ed8f3af01466b5590
Gaurav N V <gauravvenkat2k22@icloud.com>
2024-06-17 16:25:34 +0530

    feat: Booleans
    Added learnt Boolean types along with their alternate usage

---
+++ Booleans.py
@@ -0,0 +1,10 @@
+# bolleans are known as bool in python
+# they are either True or false
+
+user_name = "John"
+is_admin = False # boolean
+
+# note 0 => False, 1=> True
+
+print(bool(0)) # False
+print(bool(1)) # True
