425bfaf23f7279eeabe977cd4db9cdf38f9910be
Gaurav N V <gauravvenkat2k22@icloud.com>
2024-06-17 15:00:19 +0530

    feat: Escape sequences
    added a code files that shows some of main escape sequence used in python

---
+++ escape_sequences.py
@@ -0,0 +1,15 @@
+
+# to escape a single character use \
+weather = "It\'s \"kind of\" sunny"
+print(weather) # It's "kind of" sunny
+
+# to add a tab space use \t
+# note: 1 tab = 4 spaces
+weather = "It\'s \"kind of\" \tsunny"
+print(weather) # It's "kind of"  sunny
+
+# to add a new line use \n
+weather = "It\'s \"kind of\" \nsunny"
+print(weather) 
+# It's "kind of"  
+# sunny
