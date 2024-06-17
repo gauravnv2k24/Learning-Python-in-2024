ce59bfaec4e0c78c306a841f6060e5cb1f73624f
Gaurav N V <gauravvenkat2k22@icloud.com>
2024-06-17 14:52:51 +0530

    feat: Type conversion with example
    Added a code file that contain example to converting types along with simple int to string conversion implementation.

---
+++ type_conversions.py
@@ -0,0 +1,14 @@
+# below code shows type conversion
+# str()-> to convert to string
+# list()-> to convert to list
+# dict()-> to convert to dict
+# int()-> to convert to int
+# float()-> to convert to float
+
+
+# int to string conversion implementation
+num = 100
+print(type(num)) # <class 'int'>
+
+converted = str(num) # converting int to string
+print(type(converted))# <class 'str'>
